abbbff4e8fa5eee2c8992f0818e6c47d38ccd293
Rennan <rennanxsz@gmail.com>
2021-04-13 23:22:10 -0300

    Regras do jogo incluidas junto com o resultado do vencedor.

--- app/src/main/java/com/example/pedrapapeltesoura2/MainActivity.java
+++ app/src/main/java/com/example/pedrapapeltesoura2/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.sql.SQLOutput;
 import java.util.Random;
@@ -33,13 +35,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void selectedOption(String selectedOption){
-        System.out.println(selectedOption);
+
+        ImageView imageResult = findViewById(R.id.imageResult);
+        TextView textResult = findViewById(R.id.textResult);
 
         int number = new Random().nextInt(3);
-        String[] options = { "stone", "paper", "scissors"};
-        String appOption = options[ number ];
+        String[] options = { "Stone", "Paper", "Scissors"};
+        String appOptions = options[ number ];
 
-        System.out.println(appOption);
+        switch ( appOptions ){
+            case "Stone":
+                imageResult.setImageResource(R.drawable.pedra);
+                break;
+            case "Paper":
+                imageResult.setImageResource(R.drawable.papel);
+                break;
+            case "Scissors":
+                imageResult.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+        if (
+                (appOptions == "Paper" && selectedOption == "Stone")||
+                (appOptions == "Stone" && selectedOption == "Scissors")||
+                (appOptions == "Scissors" && selectedOption == "Paper")
+        ){//App Winner
+            textResult.setText("Você perdeu !");
+
+        }else if (
+                (selectedOption == "Paper" && appOptions == "Stone")||
+                (selectedOption == "Stone" && appOptions == "Scissors")||
+                (selectedOption == "Scissors" && appOptions == "Paper")
+        ){//User Winner
+            textResult.setText("Você Ganhou !");
+        }else{// Draw
+            textResult.setText("Empatou !");
+        }
+
+        System.out.println("item selecionado: "+ textResult);
 
 
     }
