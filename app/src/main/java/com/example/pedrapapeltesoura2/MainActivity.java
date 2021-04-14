f5c6a15b831fe772778d7c61da8be3eb6493a810
Rennan <rennanxsz@gmail.com>
2021-04-13 22:31:07 -0300

    Criando opções do app, e metodos onClic dos botões de seleção do usuário.

--- app/src/main/java/com/example/pedrapapeltesoura2/MainActivity.java
+++ app/src/main/java/com/example/pedrapapeltesoura2/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.pedrapapeltesoura2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.sql.SQLOutput;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void selectedStone(View view){
+        this.selectedOption("Stone");
+
+    }
+
+    public void selectedPaper(View view){
+        this.selectedOption("Paper");
+
+    }
+
+    public void selectedScissors(View view){
+        this.selectedOption("Scissors");
+
+    }
+
+    public void selectedOption(String selectedOption){
+        System.out.println(selectedOption);
+
+        int number = new Random().nextInt(3);
+        String[] options = { "stone", "paper", "scissors"};
+        String appOption = options[ number ];
+
+        System.out.println(appOption);
+
+
+    }
+
 }
